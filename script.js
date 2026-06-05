b7839806412bd7b7b867e408e8a47572c4e0652b
dineshgnanavel01-code <dineshgnanavel01@gmail.com>
2026-06-05 16:30:17 +0530

    adding index.html

---
+++ script.js
@@ -0,0 +1,76 @@
+//variables
+
+// Creating variables
+let studentName = "Dinesh";
+let studentAge = 26;
+let studentCourse = "Frotend";
+
+// Printing values
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+// Creating variables
+var company = "Stackly";
+let salary = 30000;
+const country = "India";
+
+// Update
+company = "Google";
+salary = 30000; // same value, but reassigned
+
+// Printing all values
+console.log(company);
+console.log(salary);
+console.log(country);
+
+
+// Taking input from the user
+let userName = ("Dinesh");
+
+// Printing the welcome message
+console.log("Welcome " + userName);
+
+// Asking the user with confirm()
+let knowsJS = ("Do you know JavaScript?");
+
+// Printing the result
+console.log(knowsJS);
+
+//variables
+StudentName="Dinesh"
+StudentAge="26"
+StudentCity="Salem"
+
+// Printing all values
+console.log(StudentName);
+console.log(StudentAge);
+console.log(StudentCity);
+
+//variables
+let course = "JavaScript";
+
+// Showing the alert message
+console.log("Welcome to " + course)
+
+// Storing favorites in variables
+let favoriteFood = "Chicken Biryani";
+let favoriteColor = "Black";
+let favoriteMovie = "Leo";
+
+// Printing values
+console.log("Food : " + favoriteFood);
+console.log("Color : " + favoriteColor);
+console.log("Movie : " + favoriteMovie);
+
+//variables
+Name="Kannan"
+Age="26"
+Phone="Salem"
+Email="dinesh34@gmail.com"
+// Printing valu
+console.log(Name);
+console.log(Age);
+console.log(Phone);
+console.log(Email);
+
